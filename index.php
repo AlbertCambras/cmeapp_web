2619d48df04c91b165e832cba7b2c24e45ce577d
AlbertCambras <acambras@wwwarcelona.com>
2023-09-11 12:42:10 +0200

    fix styles, date

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
+    <title>CMEApp</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
@@ -19,16 +19,16 @@
         }
         ?>
 
-        <h1>Formulario</h1>
+        <h1>Ingresar datos</h1>
         <form action="get_data.php" method="post">
-            <label for="dni">DNI:</label>
-            <input type="text" id="dni" name="dni" required><br><br>
-
-            <label for="name">Nombre:</label>
+            <label for="name">NAME:</label>
             <input type="text" id="name" name="name" required><br><br>
 
+            <label for="name">DNI:</label>
+            <input type="text" id="dni" name="dni" required><br><br>
+
             <label for="date">Fecha:</label>
-            <input type="date" id="date" name="date" required><br><br>
+            <input type="date" id="fecha" name="fecha" value="<?php echo date('Y-m-d'); ?>" ><br><br>
 
             <input type="submit" value="Enviar">
         </form>
